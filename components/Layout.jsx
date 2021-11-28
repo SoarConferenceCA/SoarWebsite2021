92a06e448376e5b8acdd1fade4d098d2165175fc
Kam S <singhkam07@gmail.com>
2021-11-28 17:03:19 -0500

    Initial components, layout, app, and index file

---
+++ components/Layout.jsx
@@ -0,0 +1,14 @@
+import NavBar from './NavBar';
+import Footer from './Footer';
+
+export default function Layout({ children }) {
+    return (
+        <>
+            <NavBar />
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </>
+    );
+}
---
+++ components/NavBar.jsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+    return (
+        <div>
+            This is a navbar.
+        </div>
+    )
+}
