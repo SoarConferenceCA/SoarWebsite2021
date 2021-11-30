7b8225264042defe95b02ee496f4381456a59bec
Kam S <singhkam07@gmail.com>
2021-11-30 09:24:11 -0500

    Added meta viewport tag to allow for responsive design

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,14 @@
 import NavBar from './NavBar';
 import Footer from './Footer';
+import Head from 'next/head';
 
 export default function Layout({ children }) {
     return (
         <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </Head>
+
             <NavBar />
             <main>
                 {children}
