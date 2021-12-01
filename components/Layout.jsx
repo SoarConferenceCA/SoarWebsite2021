ca6621dfe21ed3b94d13dda9f5c018e358dd3229
Kam S <singhkam07@gmail.com>
2021-11-30 21:58:29 -0500

    Updated Layout to include routes and social links

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,22 @@
 import NavBar from './NavBar';
 import Footer from './Footer';
 import Head from 'next/head';
+import { links } from './routes';
+import socials from './socials';
 
 export default function Layout({ children }) {
+    console.log(socials);
     return (
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
 
-            <NavBar />
+            <NavBar links={links} />
             <main>
                 {children}
             </main>
-            <Footer />
+            <Footer links={links} socials={socials} />
         </>
     );
 }
