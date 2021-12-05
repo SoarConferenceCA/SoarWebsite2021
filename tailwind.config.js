8de3d0c86d29346ce53f4add77d2f2c8f9fb4448
Kam S <singhkam07@gmail.com>
2021-12-05 11:59:55 -0500

    Updated tailwind colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,24 @@ module.exports = {
     extend: {
       colors: {
         lime: '#a4beb8',
-        'pale-pink': '#e6a195'
+        'pale-pink': '#e6a195',
+        'weird-grey': '#f7f6f4',
+        'odd-green': '#a4beb8'
+      },
+      backgroundImage: {
+        'unknown': "url('/assets/unknown.png')"
+      },
+      width: {
+        'w360': '360px'
+      },
+      height: {
+        'ab10': '480px',
+        'ab4': '360px',
+        'lb4': 'auto',
+        '320': '320px'
+      },
+      margin: {
+        '-100': '-100%'
       }
     },
   },
