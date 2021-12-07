abfa422555db77acf48071f3017ea3c3e7a5adec
Kam S <singhkam07@gmail.com>
2021-12-07 14:19:20 -0500

    tailwind settings + global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,15 @@ module.exports = {
     extend: {
       colors: {
         lime: '#a4beb8',
-        'pale-pink': '#e6a195',
-        'weird-grey': '#f7f6f4',
-        'odd-green': '#a4beb8'
+        'pale-pink': '#dd7766',
+        'weird-grey': '#e5e5e5',
+        'odd-green': '#8fb0a9'
       },
       backgroundImage: {
         'unknown': "url('/assets/unknown.png')"
       },
       width: {
-        'w360': '360px'
+        'w360': '360px',
       },
       height: {
         'ab10': '480px',
@@ -25,6 +25,10 @@ module.exports = {
       },
       margin: {
         '-100': '-100%'
+      },
+      maxWidth: {
+        'w370': '370px',
+        'w280': '280px'
       }
     },
   },
