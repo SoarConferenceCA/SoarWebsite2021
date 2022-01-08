6ec028a0b8b3a816b14e0b87f75cc9eecb984e31
Kam S <singhkam07@gmail.com>
2022-01-08 12:54:18 -0500

    Initial Members info

--- data/membersdata.js
+++ data/membersdata.js
@@ -13,6 +13,218 @@ const x = [
     "http://facebook.com"
 ];
 
+module.exports.portfolios = [
+    {
+        name: "MLD",
+        members: [
+            {
+                firstname: "Naythan",
+                lastname: "Jayamaha",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/naythan.jay/"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: "Sponsorship",
+        members: [
+            {
+                firstname: "Komal",
+                lastname: "Jambusaria",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/komal_j8/"
+                    }
+                ]
+            },
+            {
+                firstname: "Anmol",
+                lastname: "Bal",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/anmol_290/"
+                    }
+                ]
+            },
+            {
+                firstname: "Mohamed",
+                lastname: "Alinur",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://www.instagram.com/mohamedaalinur/"
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        name: "Creative Director",
+        members: [
+            {
+                firstname: "Tianni",
+                lastname: "Zhao",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/tiannizhao/"
+                    },
+                ]
+            },
+            {
+                firstname: "Ishleen",
+                lastname: "Wadehra",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/ishleen_wdh/"
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        name: "Entertainment",
+        members: [
+            {
+                firstname: "",
+                lastname: "",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: ""
+                    }
+                ]
+            },
+            {
+                firstname: "Aliyah",
+                lastname: "Yagboyaju",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://www.instagram.com/_.aliyxh/"
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        name: "Junior Rep",
+        members: [
+            {
+                firstname: "Yugma",
+                lastname: "Vyas",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/yugmav_/"
+                    }
+                ]
+            },
+            {
+                firstname: "Abigail",
+                lastname: "Shibu",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/abiigailshibu/"
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        name: "Volunteer Head (CCOM)",
+        members: [
+            {
+                firstname: "Tanya",
+                lastname: "Puri",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/tanya_puri22"
+                    }
+                ]
+            },
+            {
+                firstname: "Neiloy",
+                lastname: "Chaudhuri",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/_neiloy/"
+                    }
+                ]
+            },
+            {
+                firstname: "Armaan",
+                lastname: "Gill",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/armaan_gill18/"
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        name: "Chair",
+        members: [
+            {
+                firstname: "Rhea",
+                lastname: "Nijjer",
+                image: "/assets/anon.jpg",
+                socials: [
+                    {
+                        type: mediaTypes.INSTAGRAM,
+                        link: "https://instagram.com/rheanijjer/"
+                    }
+                ]
+            },
+        ]
+    }
+];
+
+
+// {
+//     name: "",
+//     members: [
+
+//     ]
+// },
+
+// {
+//     firstname: "",
+//     lastname: "",
+//     image: "/assets/anon.jpg",
+//     socials: [
+//         {
+//             type: mediaTypes.INSTAGRAM,
+//             link: ""
+//         }
+//     ]
+// },
+
 /*
 
 Portfolio {
@@ -34,25 +246,5 @@ Portfolio {
 }
 portfolio.members.length > 2 ? 2 : portfolio.members.length
 */
-
-module.exports.portfolios = [...Array(6)].map( (_, idx) => {
-    return {
-        name: `Portfolio ${idx}`,
-        members: [...Array(idx+1)].map( (_, gidx) => {
-            return {
-                firstname: `Member${gidx}`,
-                lastname: `Last${gidx}`,
-                position: `Some Position ${gidx}`,
-                image: "/assets/anon.jpg",
-                socials: [...Array(3)].map( (_, yidx) => {
-                    return {
-                        type: yidx,
-                        link: x[yidx]
-                    }
-                })
-            }
-        })
-    }
-});
 
 module.exports.mediaTypes = mediaTypes;
